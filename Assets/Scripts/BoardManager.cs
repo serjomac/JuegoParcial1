ddcfc1557c2bdeae316c6dcc8cc99014e7145dae
Jonathan Macías <jonathan.macias@qbit.com.ec>
2019-06-25 23:00:16 -0500

    Avance de juego

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    public static BoardManager shareInstace;
+    public List<Sprite> prefab = new List<Sprite>();
+    public GameObject currentCandy;
+    public int xSize, ySize;
+
+    public bool isShifting { get; set; }
+
+    private Candy selectedCandy; 
+
+    private GameObject[,] candies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (shareInstace == null)
+        {
+            shareInstace = this;
+        }else
+        {
+            Destroy(gameObject);
+        }
+        Vector2 offset = currentCandy.GetComponent<BoxCollider2D>().size;
+        CreateInitialBoard(offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void CreateInitialBoard(Vector2 offset){
+        candies = new GameObject[xSize, ySize];
+
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+
+        int idx = -1;
+        
+        for (int x = 0; x < xSize ; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newCandy = Instantiate(currentCandy, new Vector3(startX + (offset.x * x), startY + (offset.y * y),
+                                0), currentCandy.transform.rotation);
+                newCandy.name = string.Format("Candy[{0}][{1}]", x, y);
+                
+
+                do {
+                    idx = Random.Range(0, prefab.Count);
+                } while (
+                        (x > 0 && idx == candies[x-1, y].GetComponent<Candy>().id) ||
+                        (y > 0 && idx == candies[x, y-1].GetComponent<Candy>().id)
+                );
+                Sprite sprite = prefab[idx];
+                newCandy.GetComponent<SpriteRenderer>().sprite = sprite;
+                newCandy.GetComponent<Candy>().id = idx;
+                newCandy.transform.parent = this.transform;
+                candies[x, y] = newCandy;
+            }
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private static Candy previousSelected = null;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+
+    public int id;
+    
+    private Vector2[] adjacentDirections = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void SelectCandy()
+    {
+        isSelected = true;
+        spriteRenderer.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Candy>();
+    }
+
+    private void DeselectCandy()
+    {
+        isSelected = false;
+        spriteRenderer.color = Color.white;
+        previousSelected = null;
+    }
+
+    private void OnMouseDown()
+    {
+        if(spriteRenderer.sprite == null || 
+            BoardManager.shareInstace.isShifting)
+        {
+            return;
+        }
+
+        if (isSelected)
+        {
+            DeselectCandy();
+        }
+        else
+        {
+            if(previousSelected == null)
+            {
+                SelectCandy();
+            }else
+            {
+                previousSelected.DeselectCandy();
+                SelectCandy();
+            }
+
+            /* 
+            else
+            {
+                 if (CanSwipe())
+                {
+                    SwapSprite(previousSelected);
+                    previousSelected.FindAllMatches();
+                    previousSelected.DeselectCandy();
+                    FindAllMatches();
+
+                    GUIManager.shareInstace.MoveCounter--;
+                }
+                else
+                {
+                    previousSelected.DeselectCandy();
+                    SelectCandy();
+                }
+                */
+
+            
+        }
+    }
+}
